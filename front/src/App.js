3ff705b81b11a61a280d58b566c81325c12ab903
Dariel Ramos <dariel.ramos.dv@gmail.com>
2022-01-15 00:06:22 -0500

    add bootstrap and react-bootstrap

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
@@ -9,14 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Button>Learn Bootstrap</Button>
       </header>
     </div>
   );
