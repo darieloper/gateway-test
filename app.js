3687ad0b411b51a310601c39c8c51f5da939fffb
Dariel Ramos <73610976+darieloper@users.noreply.github.com>
2022-01-17 03:26:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,44 +11,11 @@ module.exports = class App {
   constructor(port, controllers) {
     this.port = port;
     this.app = express();
-    this.controllers = controllers || [];
-    console.log('----------------------constructor-----------------');
-
-    this.initializeMiddlewares();
-    this.initializeControllers();
-    console.log('-----------------before---------------------------');
-    this.initializeStaticResources();
-    console.log('-----------------after-----------------------------');
-  }
-
-  initializeMiddlewares() {
-    this.app.use(cors());
-    this.app.use(express.json());
-
-    if (process.env.NODE_ENV !== 'test') {
-      this.app.use(logger);
-    }
-  }
-
-  initializeStaticResources() {
-    console.log(process.env.NODE_ENV);
-    /*if (!'production'.includes(process.env.NODE_ENV)) {
-      return
-    }*/
-
-    console.log('starting serving static files');
+    
     this.app.use(express.static(path.join(__dirname, 'front', 'build')));
-
-    console.log('starting redirecting for react router');
-    this.app.get('/*', function (req, res) {
+    this.app.get('*', function (req, res) {
       res.sendFile(path.join(__dirname, 'front', 'build', 'index.html'));
     });
-
-    console.log('end static resources initialation');
-  }
-
-  initializeControllers() {
-    this.controllers.forEach((controller) => this.app.use(this.basePath + controller.path, controller.router))
   }
 
   listen() {
@@ -59,7 +26,6 @@ module.exports = class App {
 
   run() {
     const runApp = async () => {
-      await mongoose.connect(AppConfig.MONGODB_CONNECTION);
       this.listen();
     }
 
